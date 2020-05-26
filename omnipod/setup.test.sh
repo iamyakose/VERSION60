c86002b0ab4f61184149c5e7c329527949ba92c3
yakose <yakose.soa@gmail.com>
2020-05-26 12:48:22 +0200

    Add text test env out to CI Server

--- omnipod/setup.test.sh
+++ omnipod/setup.test.sh
@@ -6,8 +6,5 @@ while ! nc -z postgres 5432; do
   sleep 0.1
 done
 
-echo "PostgreSQL started"
+echo "You should I Have Started :PostgreSQL"
 
-#bin/rails db:create RAILS_ENV=test
-#bin/rails db:migrate RAILS_ENV=test
-#bin/rake RAILS_ENV=test
